1dd63f2cecc9a544fa385739ff70d67f9396ca44
Marcel van Breeden <marcelvanbreeden@Marcels-MacBook-Air.local>
2016-01-16 22:29:41 +0100

    cleaned up functions, created main()
    Time to clean up the code, before starting more OOP coding

--- RCTruck.py
+++ RCTruck.py
@@ -5,7 +5,7 @@ import atexit
 import cwiid, time
 
 #version
-version = "MvB0.014"
+version = "MvB0.015"
 
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT(addr=0x60)
@@ -22,29 +22,17 @@ def turnOffMotors():
 	#unused mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
 	#unused mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
-def turnLeft():
+def turn(direction):
 	if motorSpeed > 0:
-		motors['left'].run(Adafruit_MotorHAT.BACKWARD);
-		motors['left'].setSpeed(motorSpeed)
+		motors[direction].run(Adafruit_MotorHAT.BACKWARD);
+		motors[direction].setSpeed(motorSpeed)
 		time.sleep(0.3)
-		motors['left'].run(Adafruit_MotorHAT.FORWARD);
+		motors[direction].run(Adafruit_MotorHAT.FORWARD);
 	else:
-		motors['left'].run(Adafruit_MotorHAT.FORWARD);
-                motors['left'].setSpeed(motorSpeed)
+		motors[direction].run(Adafruit_MotorHAT.FORWARD);
+                motors[direction].setSpeed(motorSpeed)
                 time.sleep(0.3)
-                motors['left'].run(Adafruit_MotorHAT.BACKWARD);
-
-def turnRight():
-	        if motorSpeed > 0:
-                motors['right'].run(Adafruit_MotorHAT.BACKWARD);
-                motors['right'].setSpeed(motorSpeed)
-                time.sleep(0.3)
-                motors['right'].run(Adafruit_MotorHAT.FORWARD);
-        else:
-                motors['right'].run(Adafruit_MotorHAT.FORWARD);
-                motors['right'].setSpeed(motorSpeed)
-                time.sleep(0.3)
-                motors['right'].run(Adafruit_MotorHAT.BACKWARD);
+                motors[direction].run(Adafruit_MotorHAT.BACKWARD);
 
 def setNewMotorSpeed(motorSpeed):
 	if leftMotorStarted:
@@ -64,7 +52,7 @@ def setNewMotorSpeed(motorSpeed):
                 else:
 			print('Right Going backward')
                         motors['right'].run(Adafruit_MotorHAT.BACKWARD)
- 
+
 		motors['right'].setSpeed(abs(motorSpeed))
 
 def rumbleWii():
@@ -77,138 +65,129 @@ def closeWiiConnection():
 	wii.close()
 
 def startStopMotor(motor):
-	if not rightMotorStarted:
-		print('Starting right engine')
-		motor.setSpeed(startSpeed)
-		motor.run(Adafruit_MotorHAT.FORWARD);
-		rightMotorStarted = True
+	if not motorStarted[motor]:
+		print('Starting %s engine' & motor)
+		motors[motor].setSpeed(startSpeed)
+		motors[motor].run(Adafruit_MotorHAT.FORWARD);
+		motorStarted[motor] = True
 	else:
-		print('Stopping right engine')
-		motors['right'].run(Adafruit_MotorHAT.RELEASE);
-		rightMotorStarted = False
+		print('Stopping %s engine' & motor)
+		motors[motor].run(Adafruit_MotorHAT.RELEASE);
+		motorStarted[motor] = False
 
-#make sure that engines are turned off at any exit
-atexit.register(turnOffMotors)
+def main():
+	#make sure that engines are turned off at any exit
+	atexit.register(turnOffMotors)
 
-print('Welcome to RCTruck %s' % version)
-#connect and initiatie Wiimote
-print('Please press buttons 1 + 2 on your Wiimote now ...')
-time.sleep(2)
+	print('Welcome to RCTruck %s' % version)
+	#connect and initiatie Wiimote
+	print('Please press buttons 1 + 2 on your Wiimote now ...')
+	time.sleep(2)
 
-# This code attempts to connect to your Wiimote and if it fails the program quits
-try:
-	global wii
-	wii = cwiid.Wiimote()
-except RuntimeError:
-	print('Cannot connect to your Wiimote. Run again and make sure you are holding buttons 1 + 2!')
-	exit()
-
-print('Wiimote connection established!\n')
-rumbleWii()
-print('Press PLUS and MINUS together to disconnect and quit.\n')
-
-#Only start the engines when wiimote is connected successfully
-motors = { 'left' : mh.getMotor(1), 'right' : mh.getMotor(2)}
-
-# turn on left and right motors
-motors['left'].run(Adafruit_MotorHAT.RELEASE);
-motors['right'].run(Adafruit_MotorHAT.RELEASE);
-
-time.sleep(1)
-
-wii.rpt_mode = cwiid.RPT_BTN
-
-leftMotorStarted = False
-rightMotorStarted = False
-motorSpeed = startSpeed
-
-while True:
-
-	buttons = wii.state['buttons']
-
-	# Detects whether + and - are held down and if they are it quits the program
-	if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
-		print('\nClosing connection ...')
-		closeWiiConnection()
-		turnOffMotors()
+	# This code attempts to connect to your Wiimote and if it fails the program quits
+	try:
+		global wii
+		wii = cwiid.Wiimote()
+	except RuntimeError:
+		print('Cannot connect to your Wiimote. Run again and make sure you are holding buttons 1 + 2!')
 		exit()
 
-	# The following code detects whether any of the Wiimotes buttons have been pressed and then prints a statement to the screen!
-	if (buttons & cwiid.BTN_LEFT):
-		print('Slowing down!')
-		motorSpeed -= 50
-		print('New speed: %i' % motorSpeed)
+	print('Wiimote connection established!\n')
+	rumbleWii()
+	print('Press PLUS and MINUS together to disconnect and quit.\n')
 
-		setNewMotorSpeed(motorSpeed)
-		time.sleep(button_delay)
+	#Only start the engines when wiimote is connected successfully
+	motors = { 'left' : mh.getMotor(1), 'right' : mh.getMotor(2)}
+	motorStarted = { 'left': False, 'right' : False}
 
-	if(buttons & cwiid.BTN_RIGHT):
-		print('Speeding up!')
-		motorSpeed += 50
-		print('New speed: %i' % motorSpeed)
+	# turn on left and right motors
+	motors['left'].run(Adafruit_MotorHAT.RELEASE);
 
-		setNewMotorSpeed(motorSpeed)
-		time.sleep(button_delay)
+	motors['right'].run(Adafruit_MotorHAT.RELEASE);
 
-	# Go Left by switching left engine in backup for 1 second
-	if (buttons & cwiid.BTN_UP):
-		print 'Turning left'
-		turnLeft()
-		time.sleep(button_delay)
+	time.sleep(1)
 
-	# Go Right by switching right engine in backup for 1 second
-	if (buttons & cwiid.BTN_DOWN):
-		print 'Turning right'
-		turnRight()
-		time.sleep(button_delay)
+	wii.rpt_mode = cwiid.RPT_BTN
 
-	if (buttons & cwiid.BTN_1):
-		if not leftMotorStarted:
-			print('Starting left engine')
-			motors['left'].setSpeed(startSpeed)
-			motors['left'].run(Adafruit_MotorHAT.FORWARD);
-			leftMotorStarted = True
-		else:
-			print('Stopping left engine')
-			motors['left'].run(Adafruit_MotorHAT.RELEASE);
-			leftMotorStarted = False
+	leftMotorStarted = False
+	rightMotorStarted = False
+	motorSpeed = startSpeed
 
-		time.sleep(button_delay)
+	while True:
 
-	if (buttons & cwiid.BTN_2):
-		if not rightMotorStarted:
-			print('Starting right engine')
-			motors['right'].setSpeed(startSpeed)
-			motors['right'].run(Adafruit_MotorHAT.FORWARD);
-			rightMotorStarted = True
-		else:
-			print('Stopping right engine')
-			motors['right'].run(Adafruit_MotorHAT.RELEASE);
-			rightMotorStarted = False
+		buttons = wii.state['buttons']
 
-		time.sleep(button_delay)
+		# Detects whether + and - are held down and if they are it quits the program
+		if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
+			print('\nClosing connection ...')
+			closeWiiConnection()
+			turnOffMotors()
+			exit()
 
-	if (buttons & cwiid.BTN_A):
-		print 'Toettoet!'
-		time.sleep(button_delay)
+		# The following code detects whether any of the Wiimotes buttons have been pressed and then prints a statement to the screen!
+		if (buttons & cwiid.BTN_LEFT):
+			print('Slowing down!')
+			motorSpeed -= 50
+			print('New speed: %i' % motorSpeed)
 
-	if (buttons & cwiid.BTN_B):
-		rumbleWii()
-		time.sleep(button_delay)
+			setNewMotorSpeed(motorSpeed)
+			time.sleep(button_delay)
 
-	if (buttons & cwiid.BTN_HOME):
-		wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
-		check = 0
-		while check == 0:
-			print(wii.state['acc'])
-			time.sleep(0.01)
-			check = (buttons & cwiid.BTN_HOME)
-		time.sleep(button_delay)
+		if(buttons & cwiid.BTN_RIGHT):
+			print('Speeding up!')
+			motorSpeed += 50
+			print('New speed: %i' % motorSpeed)
 
-	if (buttons & cwiid.BTN_MINUS):
-		#print 'Left pressed'
-		time.sleep(button_delay)
+			setNewMotorSpeed(motorSpeed)
+			time.sleep(button_delay)
 
-	if (buttons & cwiid.BTN_PLUS):
-		#  print 'Right pressed'
-		time.sleep(button_delay)
+		# Go Left by switching left engine in backup for 1 second
+		if (buttons & cwiid.BTN_UP):
+			print 'Turning left'
+			turn('left')
+			time.sleep(button_delay)
+
+		# Go Right by switching right engine in backup for 1 second
+		if (buttons & cwiid.BTN_DOWN):
+			print 'Turning right'
+			turn('right')
+			time.sleep(button_delay)
+
+		# Start or stop the left engine
+		if (buttons & cwiid.BTN_1):
+			startStopMotor('left')
+			time.sleep(button_delay)
+
+		# Start or stop the right engine
+		if (buttons & cwiid.BTN_2):
+			startStopMotor('right')
+			time.sleep(button_delay)
+
+		if (buttons & cwiid.BTN_A):
+			print 'Toettoet!'
+			time.sleep(button_delay)
+
+		if (buttons & cwiid.BTN_B):
+			rumbleWii()
+			time.sleep(button_delay)
+
+		if (buttons & cwiid.BTN_HOME):
+			wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+			homeButtonStillPressed = 0
+			while homeButtonStillPressed == 0:
+				print(wii.state['acc'])
+				time.sleep(0.01)
+				homeButtonStillPressed = (buttons & cwiid.BTN_HOME)
+			time.sleep(button_delay)
+
+		if (buttons & cwiid.BTN_MINUS):
+			#print 'Left pressed'
+			time.sleep(button_delay)
+
+		if (buttons & cwiid.BTN_PLUS):
+			#  print 'Right pressed'
+			time.sleep(button_delay)
+
+
+if __name__ == "__main__":
+    main()
