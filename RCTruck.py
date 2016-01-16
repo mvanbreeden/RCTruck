d9e0fb5b1c0043d83b5ad6edcdd83ffa1f1f7900
Marcel van Breeden <marcelvanbreeden@Marcels-MacBook-Air.local>
2016-01-16 22:58:28 +0100

    just small license, not all the way gnu

--- RCTruck.py
+++ RCTruck.py
@@ -2,17 +2,11 @@
 # Copyright (c) 2016 Marcel van Breeden
 #
 # This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the added License file.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
 # For more info on Adafruit libs and set up see:
 # https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi/installing-software
